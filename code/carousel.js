2a68861108ef38bb16a5719de9ee5e9912b9a369
Jakub Marchut <kuba.marchut@gmail.com>
2022-08-10 12:53:24 +0200

    enclosing whole script inside one function, introducing first version of infinite mode, improving distance to next element

--- code/carousel.js
+++ code/carousel.js
@@ -3,96 +3,138 @@
 
 // @author Jakub Marchut <kuba.marchut@gmail.com>
 
-let verbose = true; // shows debug information in browsers console
-let mainDiv = ".elementor-widget-woolentor-universal-product"; // points to the main div which contains overflowing elements
-let delay = 2000; // set delay between each "step" ignored in marqee mode
-let pauseOnHover = true; // decide whether animation should stop when user hovers over
-let infiniteMode = false;
-let marqeeMode = true;
-let hideNotVisible = false;
+const carouselSilderModifier = () => {
+  let verbose = true; // shows debug information in browsers console
+  let mainDiv = ".elementor-widget-woolentor-universal-product"; // points to the main div which contains overflowing elements
+  let delay = 2000; // set delay between each "step" ignored in marqee mode
+  let pauseOnHover = true; // decide whether animation should stop when user hovers over
+  let infiniteMode = true;
+  let marqeeMode = true;
+  let hideNotVisible = false;
 
+  function findSectionParent(ele){
+    let parent = ele.parentNode;
+    while (parent.tagName.toLowerCase() != "section") {
+      parent = parent.parentNode;
+    }
 
-let modifiedElementorWidget = document.querySelector(mainDiv);
-if (modifiedElementorWidget != null) {
-  let modifiedElementorWidgetContainer = modifiedElementorWidget.children[0].children[0];
-  let modifiedSingleElements = modifiedElementorWidgetContainer.children;
-
-  if (marqeeMode) {
-    delay = 15;
+    return parent;
   }
-  if (infiniteMode) {
+  function removeNextSectionMargin(section){
+    let siblingSection = section.nextElementSibling;
+    console.log(siblingSection, section);
+    siblingSection.style.marginTop = "0";
+  }
+  function duplicateVisible(){
     for (var i = 0; i < modifiedSingleElements.length; i++) {
-      //modifiedSingleElements[i].style.opacity = 0;
-      modifiedSingleElements[i].style.transition = "1.2s opacity ease";
+      if (isScrolledIntoView(modifiedSingleElements[i], 0.75)) {
+        let clone = modifiedSingleElements[i].cloneNode(true);
+        modifiedElementorWidgetContainer.appendChild(clone);
+      }
+      else break;
     }
   }
-  if (hideNotVisible) {
-    modifiedElementorWidgetContainer.addEventListener("scroll", hideNotVisibleHandler)
+
+  let modifiedElementorWidget = document.querySelectorAll(mainDiv)[2];
+  let modifiedElementorWidgetContainer;
+  let modifiedSingleElements;
+  if (modifiedElementorWidget != null) {
+    modifiedElementorWidgetContainer = modifiedElementorWidget.children[0].children[0];
+    modifiedSingleElements = modifiedElementorWidgetContainer.children;
+    removeNextSectionMargin(findSectionParent(modifiedElementorWidget));
+    if (marqeeMode) {
+      delay = 1/60*1000;
+    }
+    if (hideNotVisible) {
+      for (var i = 0; i < modifiedSingleElements.length; i++) {
+        //modifiedSingleElements[i].style.opacity = 0;
+        modifiedSingleElements[i].style.transition = "0.6s opacity ease-in";
+      }
+      modifiedElementorWidgetContainer.addEventListener("scroll", hideNotVisibleHandler);
+    }
+    modifiedElementorWidgetContainer.style.flexWrap = "noWrap";
+    modifiedElementorWidgetContainer.style.overflow = "hidden";
+    let modifiedElementorWidgetGap = window.getComputedStyle(modifiedSingleElements[0]).paddingLeft;
+
+    modifiedElementorWidget.children[0].style.marginLeft = "-" + modifiedElementorWidgetGap;
+    modifiedElementorWidget.children[0].style.marginRight = "-" + modifiedElementorWidgetGap;
+    modifiedElementorWidgetContainer.style.padding = `0 ${modifiedElementorWidgetGap} ${parseInt(modifiedElementorWidgetGap)*4}px`;
+    if (infiniteMode) {
+      duplicateVisible();
+    }
+    let moveInterval = setInterval(moveProducts, delay);
+    if(pauseOnHover){
+      modifiedElementorWidget.addEventListener("mouseenter", () => {
+        clearInterval(moveInterval);
+      })
+      modifiedElementorWidget.addEventListener("mouseleave", () => {
+        moveProducts();
+        moveInterval = setInterval(moveProducts, delay);
+      })
+    }
   }
-  modifiedElementorWidgetContainer.style.flexWrap = "noWrap";
-  modifiedElementorWidgetContainer.style.overflow = "hidden";
-  let modifiedElementorWidgetGap = window.getComputedStyle(modifiedSingleElements[0]).paddingLeft;
+  else if (verbose) console.log("Main element was not found");
 
-  modifiedElementorWidget.children[0].style.marginLeft = "-" + modifiedElementorWidgetGap;
-  modifiedElementorWidget.children[0].style.marginRight = "-" + modifiedElementorWidgetGap;
-  modifiedElementorWidgetContainer.style.padding = modifiedElementorWidgetGap;
-
-  let moveInterval = setInterval(moveProducts, delay);
-  if(pauseOnHover){
-    modifiedElementorWidget.addEventListener("mouseenter", () => {
-      clearInterval(moveInterval);
-    })
-    modifiedElementorWidget.addEventListener("mouseleave", () => {
-      moveProducts();
-      moveInterval = setInterval(moveProducts, delay);
-    })
-  }
-}
-else if (verbose) console.log("Main element was not found");
-
-let offset = 0;
-function hideNotVisibleHandler(){
+  let offset = 0;
+  function hideNotVisibleHandler(){
   let modifiedElementorWidgetContainer = modifiedElementorWidget.children[0].children[0];
   let modifiedSingleElements = modifiedElementorWidgetContainer.children;
   for (var i = 0; i < modifiedSingleElements.length; i++) {
-    if (isScrolledIntoView(modifiedSingleElements[i])) {
+    if (isScrolledIntoView(modifiedSingleElements[i], 0.25)) {
       modifiedSingleElements[i].style.opacity = 1;
     }
     else {
       modifiedSingleElements[i].style.opacity = 0;
     }
   }
-}
-function isScrolledIntoView(ele){
-  let rect = ele.getBoundingClientRect();
-  let eleLeft = rect.left;
-  let eleRight = rect.right;
+  }
+  function isScrolledIntoView(ele, frac){
+    let rect = ele.getBoundingClientRect();
+    let eleLeft = rect.left;
+    let eleRight = rect.right;
 
-  let parentRect = ele.parentNode.getBoundingClientRect();
-  let parentLeft = parentRect.left;
-  let parentRight = parentRect.right;
+    let parentRect = ele.parentNode.getBoundingClientRect();
+    let parentLeft = parentRect.left;
+    let parentRight = parentRect.right;
 
-  // Only completely visible elements return true:
-  let isVisible = (eleLeft >= 0) && (eleRight <= ele.parentNode.innerWidth);
-  // Partially visible elements return true:
-  isVisible = eleLeft - parentRight < (-rect.width / 2) && eleRight - parentLeft > (rect.width / 2);
-  return isVisible;
+    // Only completely visible elements return true:
+    let isVisible = (eleLeft >= 0) && (eleRight <= ele.parentNode.innerWidth);
+    // Partially visible elements return true:
+    isVisible = eleLeft - parentRight < (-rect.width * frac) && eleRight - parentLeft > (rect.width * frac);
+    return isVisible;
+  }
+  function moveProducts() {
+    console.log('move');
+    let modifiedElementorWidgetContainer = modifiedElementorWidget.children[0].children[0];
+    let modifiedSingleElements = modifiedElementorWidgetContainer.children;
+    let step = parseInt(modifiedSingleElements[0].offsetWidth);
+    if (marqeeMode) {
+      step = 1;
+    }
+    let chk = modifiedElementorWidgetContainer.scrollWidth - modifiedElementorWidgetContainer.offsetWidth - offset - step;
+    if (infiniteMode && chk < step && chk > -step) {
+      setTimeout(()=>{
+        console.log(modifiedElementorWidgetContainer.offsetWidth);
+        offset = 0;
+        modifiedElementorWidgetContainer.scrollTo({left: offset});
+        console.log(modifiedElementorWidgetContainer.offsetWidth);
+      }, delay/2);
+    }
+    if (modifiedElementorWidgetContainer.scrollWidth - modifiedElementorWidgetContainer.offsetWidth - offset > 10){
+      offset += step;
+    }
+    else {
+      offset = 0;
+    }
+    if (!marqeeMode && (infiniteMode && offset != 0)) {
+      modifiedElementorWidgetContainer.scrollTo({left: offset, behavior: 'smooth'});
+    }
+    else {
+      modifiedElementorWidgetContainer.scrollTo({left: offset});
+    }
+  }
 }
-function moveProducts() {
-  let modifiedElementorWidgetContainer = modifiedElementorWidget.children[0].children[0];
-  let modifiedSingleElements = modifiedElementorWidgetContainer.children;
-  let step = parseInt(modifiedSingleElements[0].offsetWidth);
-  if (marqeeMode) {
-    step = 1;
-  }
-  if (modifiedElementorWidgetContainer.scrollWidth - modifiedElementorWidgetContainer.offsetWidth - offset > 10){
-    offset += step;
-  }
-  else offset = 0;
-  if (!marqeeMode) {
-    modifiedElementorWidgetContainer.scrollTo({left: offset, behavior: 'smooth'});
-  }
-  else {
-    modifiedElementorWidgetContainer.scrollTo({left: offset});
-  }
-}
+
+document.addEventListener('DOMContentLoaded', function() {
+  carouselSilderModifier();
+}, false);
